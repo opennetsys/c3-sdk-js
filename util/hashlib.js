e6b945ff057584067188b86dc6c70936fb6ed383
Miguel Mota <miguelmota2@gmail.com>
2018-08-29 00:52:29 -0700

    hashlib

--- util/hashlib.js
+++ util/hashlib.js
@@ -1,24 +1,27 @@
-/*
+const crypto = require('crypto')
+const { sha512_256: sha512B256 } = require('js-sha512')
+const hexutil = require('./hexutil')
 
-// New ...
-func New() hash.Hash {
-	// note: 512_256 is a bit faster on x86_64 machines
-	return sha512.New512_256()
+function generate () {
+  // note: 512_256 is a bit faster on x86_64 machines
+  return '0x' + sha512B256(crypto.randomBytes(256))
 }
 
-// Hash ...
-func Hash(data []byte) [sha512.Size256]byte {
-	return sha512.Sum512_256(data)
+function hash (data) {
+  return Buffer.from(sha512B256(data), 'hex')
 }
 
-// HashToHexString ...
-func HashToHexString(data []byte) string {
-	b := Hash(data)
-	return hexutil.EncodeToString(b[:])
+function hashToHexString (data) {
+  return hexutil.encodeToString(hash(data))
 }
 
-// IsEqual ...
-func IsEqual(hexHash string, data []byte) bool {
-	return hexHash == HashToHexString(data)
+function isEqual (hexHash, data) {
+  return hexHash === hashToHexString(data)
 }
-*/
+
+module.exports = {
+  generate,
+  hash,
+  hashToHexString,
+  isEqual
+}
--- util/hashlib_test.js
+++ util/hashlib_test.js
@@ -1,68 +1,12 @@
 const test = require('tape')
-const hash = require('./hashing')
+const hashlib = require('./hashlib')
 
 test('sdk', t => {
-  t.plan(1)
-  t.ok(true)
+  t.plan(5)
 
-  /*
-	hs := New()
-	_, ok := hs.(hash.Hash)
-	if !ok {
-		t.Errorf("expected to be instance of hash.Hash")
-
-			[]byte("hello world"),
-			"0x0ac561fac838104e3f2e4ad107b4bee3e938bf15f2b15f009ccccd61a913f017",
-		},
-	}
-
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
-			hashBytes := Hash(tt.in)
-			hexed := hexutil.EncodeToString(hashBytes[:])
-
-		{
-			[]byte("hello world"),
-			"0x0ac561fac838104e3f2e4ad107b4bee3e938bf15f2b15f009ccccd61a913f017",
-		},
-	}
-
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
-			hs := HashToHexString(tt.in)
-
-	tests := []struct {
-		in struct {
-			arg1 string
-			arg2 []byte
-		}
-		out bool
-	}{
-		{
-			struct {
-				arg1 string
-				arg2 []byte
-			}{
-				"0x1234",
-				[]byte("foo"),
-			},
-			false,
-		},
-		{
-			struct {
-				arg1 string
-				arg2 []byte
-			}{
-				"0x0ac561fac838104e3f2e4ad107b4bee3e938bf15f2b15f009ccccd61a913f017",
-				[]byte("hello world"),
-			},
-			true,
-		},
-	}
-
-	for i, tt := range tests {
-		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
-			result := IsEqual(tt.in.arg1, tt.in.arg2)
-			if result != tt.out {
-  */
+  t.equal(hashlib.generate().toString('hex').length, 66)
+  t.equal(hashlib.hash(Buffer.from('hello world')).toString('hex'), Buffer.from('0ac561fac838104e3f2e4ad107b4bee3e938bf15f2b15f009ccccd61a913f017', 'hex').toString('hex'))
+  t.equal(hashlib.hashToHexString(Buffer.from('hello world')), '0x0ac561fac838104e3f2e4ad107b4bee3e938bf15f2b15f009ccccd61a913f017')
+  t.equal(hashlib.isEqual('0x1234', Buffer.from('foo')), false)
+  t.equal(hashlib.isEqual('0x0ac561fac838104e3f2e4ad107b4bee3e938bf15f2b15f009ccccd61a913f017', Buffer.from('hello world')), true)
 })
